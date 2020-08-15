34745a3bacfc4760f1e79c058ba02c5434cbee7b
Shyam Ji <patelshyamji4@gmail.com>
2020-08-15 12:35:44 +0530

    Working code

---
+++ zuul-server/src/test/java/com/shyam/api/zuulserver/ZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shyam.api.zuulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
